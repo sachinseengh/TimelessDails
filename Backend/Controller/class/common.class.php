de0b76f0efbd0ecd14a711c886d8e9d0bb5edb56
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2024-12-12 14:24:47 +0545

    changes made

--- Backend/Controller/class/common.class.php
+++ Backend/Controller/class/common.class.php
@@ -14,13 +14,13 @@ abstract class Common
 
     public function validate($value){
         $val = htmlspecialchars($value);
-        $conn = mysqli_connect('localhost', 'root', '', 'nepalifootprints');
+        $conn = mysqli_connect('localhost', 'root', '', 'Timelessdials');
         $newValue = $conn->real_escape_string($val);
         return $newValue;
     }
 
     public function select($sql){
-        $conn = mysqli_connect('localhost', 'root', '', 'nepalifootprints');
+        $conn = mysqli_connect('localhost', 'root', '', 'Timelessdials');
         $var = $conn->query($sql);
         if($var->num_rows > 0){
             $datalist = $var->fetch_all(MYSQLI_ASSOC);
